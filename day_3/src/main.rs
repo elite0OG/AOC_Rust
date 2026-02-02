8d808e467ac5b51df049a6108e2a5ac4d9c5b2da
Elite0OG <tarunmeena2233@gmail.com>
2026-02-02 19:44:39 +0530

    fmt

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,7 +1,16 @@
 #![allow(unused)]
 use std::fs::read_to_string;
 
-
+fn main() 
+{
+    let input_string = read_to_string("input.txt").unwrap();
+    let data: Vec<String> = input_string
+                        .lines() // splits by '\n' 
+                        .map(|line| line.to_string()) .collect(); 
+    day_three_part_one(&data);
+    day_three_part_two(&data);
+}
+//helper function
 fn get_largest(data:&Vec<i32>)->usize
 {
     let mut current_largest = data[0];
@@ -17,13 +26,8 @@ fn get_largest(data:&Vec<i32>)->usize
     idx
 }
 
-fn main() 
+fn day_three_part_one(data:&Vec<String>)
 {
-    let input_string = read_to_string("input.txt").unwrap();
-    let data: Vec<String> = input_string
-                        .lines() // splits by '\n' 
-                        .map(|line| line.to_string()) .collect(); 
-
     let mut voltage_added =0;
     for i in 0..data.len()
     {
@@ -38,7 +42,7 @@ fn main()
             {
                 let edx = get_largest(&data_core[0..idx].to_vec());
                 let number = (data_core[0..idx].to_vec()[edx] * 10) + data_core[idx];
-                          println!("line no: {},voiltage: {}",i+1,number);
+                                //println!("line no: {},voiltage: {}",i+1,number);
                 voltage_added +=number;
                 break;
             }else {
@@ -46,7 +50,7 @@ fn main()
                 {
                 let edx = get_largest(&data_core[idx+1..data_core.len()].to_vec());
                 let number = (data_core[idx] * 10) + data_core[idx+1..data_core.len()].to_vec()[edx];
-                                println!("line no: {},voiltage: {}",i+1,number);
+                                //println!("line no: {},voiltage: {}",i+1,number);
 
                 voltage_added +=number;
                 break;
@@ -56,5 +60,17 @@ fn main()
         }
          
     }             
-    println!("VOLTAGE {}", voltage_added);//17554 YES 
+    println!("VOLTAGE:part-1: {}", voltage_added);//17554 YES 
+}
+
+fn day_three_part_two(data:&Vec<String>)
+{
+    let mut voltage_added =0;
+    for i in 0..data.len()
+    {
+        let mut data_core: Vec<i32> = data[i].chars().filter_map(|c| 
+                            c.to_digit(10)).map(|d| d as i32).collect();
+    }
+    println!("VOLTAGE:part-2: {}", voltage_added);//TLDR 
+
 }
